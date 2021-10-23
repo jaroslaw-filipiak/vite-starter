94ef63183efbd27d30357f9f7dd54c0b63bb45a6
Jarosław Filipiak <info@j-filipiak.pl>
2021-10-23 13:57:57 +0200

    handling gfonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      body: ['"Open Sans"'],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
